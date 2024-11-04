68af66f05b3b5d6a4fc5e590e89aa8e318625bb8
Sujith <sujithsmp@gmail.com>
2024-11-04 12:19:38 +0530

    ok

---
+++ mymaven/src/main/java/superkeyword/Child.java
@@ -0,0 +1,17 @@
+package superkeyword;
+
+public class Child extends Main
+{
+	
+		public Child()
+		{
+			super(100);
+			System.out.println("Child class ");
+		}
+
+	public static void main(String[] args) 
+	{
+		Child ch=new Child();
+	}
+
+}
